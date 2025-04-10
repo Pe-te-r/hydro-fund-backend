4be40c00bc20990d6552b306df5edb71a104606c
Pe-te-r <mburupeter8526@gmail.com>
2025-04-10 21:08:57 +0300

    better account info

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,6 +20,7 @@ export const OneUserService = async (id: string) => {
                     referred: {
                         columns: {
                             email: true,
+                            id:true,
                         }
                     },
                 }

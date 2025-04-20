6542cb5017608dbe9cd5eef318898633ab0e408b
Pe-te-r <mburupeter8526@gmail.com>
2025-04-20 04:47:57 +0300

    better ui

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,8 +1,9 @@
 import type { Context } from "hono";
 import { validate as isValidUUID } from 'uuid';
 import { disable2FaAuth, settingsServiceGet, updateUserSettings } from "./settings.service.js";
-import {  OneUserServiceId } from "../users/users.service.js";
+import { OneUserServiceId } from "../users/users.service.js";
 import { verifyTotpCode } from "../utils/totp.js";
+import { mailer } from "../utils/mailer.js";
 
 
 export const settingsController = async (c: Context) => {
@@ -11,21 +12,21 @@ export const settingsController = async (c: Context) => {
 
         if (!isValidUUID(id)) {
             return c.json(
-               { status: 'error', message: 'Invalid user ID format' },400
+                { status: 'error', message: 'Invalid user ID format' }, 400
             );
         }
         const results = await settingsServiceGet(id)
 
         if (!results) {
-            return c.json({ status: 'error', message: 'data not found' },400)
-            
+            return c.json({ status: 'error', message: 'data not found' }, 400)
+
         }
 
-        return c.json({status:'success',message:'data retrived success',data:results})
+        return c.json({ status: 'success', message: 'data retrived success', data: results })
 
 
     } catch (error) {
-        return c.json({status:'error',message:'unknow error occured'},500)
+        return c.json({ status: 'error', message: 'unknow error occured' }, 500)
     }
 }
 
@@ -41,7 +42,7 @@ export const updateSettings = async (c: Context) => {
 
         const user_exits = await OneUserServiceId(id)
         if (!user_exits) {
-            return c.json({status:'error',message:'user not found',data:false},404)
+            return c.json({ status: 'error', message: 'user not found', data: false }, 404)
         }
         const data = c.get('validatedData')
         console.log(data)
@@ -52,33 +53,36 @@ export const updateSettings = async (c: Context) => {
                 const results = await disable2FaAuth(id)
                 // const results = await updateUserSettings(id, { twoFactorEnabled: data.twoFactorEnabled })
                 console.log(results)
-                if (results) return c.json({ status: 'success', message: '2Fa disabled successfully',data:true })
-                return c.json({ status: 'error', message: '2Fa not disabled' ,data:false},400)
+                if (results) return c.json({ status: 'success', message: '2Fa disabled successfully', data: true })
+                return c.json({ status: 'error', message: '2Fa not disabled', data: false }, 400)
             }
             // if (user_exits.twoFactorEnabled) {
             //     return c.json({ status: 'success', message:'otp verified'})
             // }
             if (!user_exits.twoFactorEnabled) {
                 const results = await updateUserSettings(id, { twoFactorEnabled: true, })
-                if (results.success) return c.json({ status: 'success', message: '2Fa enabled successfully',data:true })
-                return c.json({ status: 'error', message: '2Fa not enabled' ,data:false},400)
-           
-            // check for otp failure
+                if (results.success) {
+                    await mailer.sendMail(user_exits.email, '2fa');
+                    return c.json({ status: 'success', message: '2Fa enabled successfully', data: true })
+                }
+                return c.json({ status: 'error', message: '2Fa not enabled', data: false }, 400)
+
+                // check for otp failure
             }
         }
-            else if (data.twoFactorSecretCode && !verifyTotpCode(user_exits.twoFactorSecret, data.twoFactorSecretCode)) {
-            return c.json({ status: 'error', message: '2Fa not correct verified' ,data:false},400)
-            
-    }
-  
-        const results = await updateUserSettings(id, data)
-        
+        else if (data.twoFactorSecretCode && !verifyTotpCode(user_exits.twoFactorSecret, data.twoFactorSecretCode)) {
+            return c.json({ status: 'error', message: '2Fa not correct verified', data: false }, 400)
 
-        if (results.success === false) return c.json({ status: 'error', message: 'data not update', data: false },403)
-        return c.json({status:true,message:'data update successfully',data:true},200)
-            
+        }
+
+        const results = await updateUserSettings(id, data)
+
+
+        if (results.success === false) return c.json({ status: 'error', message: 'data not update', data: false }, 403)
+        return c.json({ status: true, message: 'data update successfully', data: true }, 200)
+
     } catch (error) {
         // console.log(error)
-        return c.json({status:'error',message:'unknow error occured'},500)
+        return c.json({ status: 'error', message: 'unknow error occured' }, 500)
     }
 }
